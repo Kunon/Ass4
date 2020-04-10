7e5d03df4b58d47bcbfc666ef8ee4bd3f6719271
Kunon <61319557+Kunon@users.noreply.github.com>
2020-04-10 21:14:55 +0700

    ass4

---
+++ Ass 4.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class NewBehaviourScript : MonoBehaviour
+//{
+    //move
+    //private void Update()
+    //{
+    //if(Input.GetKey(KeyCode.W))
+    //{
+      // transform.position+=new Vector3(0f,0f,1f)*Time.deltaTime; 
+   // }
+   // if(Input.GetKey(KeyCode.S))
+    //{
+       //transform.position+=new Vector3(0f,0f,-1f)*Time.deltaTime; 
+    //}
+    //if(Input.GetKey(KeyCode.A))
+    //{
+       //transform.position+=new Vector3(-1f,0f,1f)*Time.deltaTime; 
+    //}
+    //if(Input.GetKey(KeyCode.D))
+    //{
+       //transform.position+=new Vector3(1f,0f,1f)*Time.deltaTime; 
+    //}
+  //shoot
+    //[SerializeField]GameObject bullet;
+    //private void Update() 
+    //{
+       // if(Input.GetKeyDown(KeyCode.Space))
+       // {
+          //  GameObject clone=Instantiate(bullet,transform.position,transform.rotation)as GameObject;
+       // }
+   // }
+//destroy bullet
+//[SerializePrivateVariables]float speed;
+   //void Update() 
+   // transform.Translate(0f,0f,speed*Time.deltaTime);
+   // {
+       // Destroy(gameObject);
+    //}
+   //}
+   //private void OnTriggerEnter(Collider other)
+    //{
+       //if (other.gameObject.Equals())
+       //{
+        //Debug.Log("You Win");
+       //}
+//}
